38cf4ab148e0a32b4cd837e273be0dcd52e9877a
root <oganessone718@gmail.com>
2024-01-05 06:15:06 +0000

    modified back init

--- server/login.js
+++ server/login.js
@@ -1,5 +1,5 @@
 const { accessToken } = req.body;
 
 const { data } = await axios.get(
-  `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${accessToken}`
-);
+            `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${accessToken}`
+        );
